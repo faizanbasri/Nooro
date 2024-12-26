bfd72a1dde2bb2d346c99575265d6859db8912d2
Faizan <faizan@codefish.com>
2024-12-26 07:11:50 +0500

    alignment in response to result

--- app/src/main/java/com/nooro/core/networkCall/responseToResult.kt
+++ app/src/main/java/com/nooro/core/networkCall/responseToResult.kt
@@ -7,16 +7,17 @@ import retrofit2.Response
 inline fun <reified T> responseToResult(response: Response<T>): Result<T, NetworkError> {
     return when (response.code()) {
         in 200..399 -> {
-             if (response.body() == null) {
-                    Result.Error(NetworkError.UNKNOWN)
-                }
-             
+            if (response.body() == null) {
+                Result.Error(NetworkError.UNKNOWN)
+            }
+
             try {
-                    Result.Success(response.body()!!)
+                Result.Success(response.body()!!)
             } catch (e: Exception) {
                 Result.Error(NetworkError.UNKNOWN)
             }
         }
+
         400 -> Result.Error(NetworkError.NO_LOCATION_FOUND)
         401 -> Result.Error(NetworkError.IN_VALID_API_KEY)
         403 -> Result.Error(NetworkError.TOO_MANY_REQUEST)
